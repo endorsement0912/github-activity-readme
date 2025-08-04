20d35b4ad25c98084bead59fe79b23456e319f13
Leeseo Ryu <ef032550@naver.com>
2025-08-04 15:42:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -156,14 +156,14 @@ const serializers = {
     )} in ${toUrlFormat(item.repo.name)}`;
   },
   PullRequestEvent: (item) => {
-    const prNum = item.payload.pull_request.number;
-    const prUrl = item.payload.pull_request.html_url;
-    const action = item.payload.pull_request.merged ? "Merged" : capitalize(item.payload.action);
-    const emoji = item.payload.pull_request.merged ? "🎉" : "💪";
-    const repoName = item.repo.name;
+  const prNum = item.payload.pull_request.number;
+  const repoName = item.repo.name;
+  const action = item.payload.pull_request.merged ? "Merged" : capitalize(item.payload.action);
+  const emoji = item.payload.pull_request.merged ? "🎉" : "💪";
 
-    return `${emoji} ${action} PR <a href="${prUrl}">#${prNum}</a> in ${repoName}`;
+  return `${emoji} ${action} PR #${prNum} in ${repoName}`;
 };
+
 
 
 Toolkit.run(
